bc305826071a05144189835634ff04a03bbc4a59
StudentDaniil <99341865+StudentDaniil@users.noreply.github.com>
2022-12-03 16:07:57 +0300

    Add files via upload

---
+++ Battle City/src/com/GvozdevDaniil/game/level/TileType.java
@@ -0,0 +1,37 @@
+package com.GvozdevDaniil.game.level;
+
+public enum TileType {
+    EMPTY(0),
+    BRICK(1),
+    METAL(2),
+    WATER(3),
+    GRASS(4),
+    ICE(5);
+
+    private int n;
+    TileType(int n){
+        this.n = n;
+    }
+
+
+    public int num(){
+        return n;
+    }
+
+    public static TileType fromNum(int n){
+        switch (n){
+            case 1:
+                return BRICK;
+            case 2:
+                return METAL;
+            case 3:
+                return WATER;
+            case 4:
+                return GRASS;
+            case 5:
+                return ICE;
+            default:
+                return EMPTY;
+        }
+    }
+}
